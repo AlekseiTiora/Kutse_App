82dc125b0e92e9cb167c6c1fb45092e7577b18f8
Opilane TTHK <opilane@tthk.ee>
2022-02-22 11:40:14 +0200

    Add project files.

---
+++ Kutse_App/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Kutse_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Kutse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind oma peole! Palun tule kindlasti";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Tere hommikust":"Tere päevast";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Ankeet(Guest guest)
+        {
+            E_mail(guest);
+            if(ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "programmeeriminetthk2@gmail.com";
+                WebMail.Password = "2.kuursus tarpv20";
+                WebMail.From = "programmeeriminetthk2@gmail.com";
+                WebMail.Send("aleksei.tiora@gmail.com", "Vastus kutsele ", guest.Name + "vastas" + ((guest.WillAttend ?? false) ? " tuleb peole " : " ei tule peole "));
+                ViewBag.Message = "Kiri on saatnud";
+
+            }
+            catch(Exception)
+            {
+                ViewBag.Message = "Mul on kahju!Ei saa kirja saada!";
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
